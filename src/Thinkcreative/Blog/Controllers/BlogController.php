b99e2f953a0d9c72dbf325ecd81e8c090a5ada21
thinkcreative-matt <56924270+thinkcreative-matt@users.noreply.github.com>
2019-10-25 14:18:20 +0100

    update controllers

--- src/Thinkcreative/Blog/Controllers/BlogController.php
+++ src/Thinkcreative/Blog/Controllers/BlogController.php
@@ -3,11 +3,29 @@
 namespace Thinkcreative\Blog\Controllers;
 
 use App\Http\Controllers\Controller;
+use Thinkcreative\Blog;
 
 class BlogController extends Controller {
 
-    public function index() {
+
+	/**
+	 * WE only need to disply the blog
+	 * @return [Array] [return a collection of blog posts]
+	 */
+    public function index() 
+    {
+
         return 'Think!';
+
+    }
+
+    /**
+     * Show the individual blog post
+     * @return [Array] [A single collection of the requested blog post]
+     */
+    public function show(Blog $post)
+    {
+
     }
 
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,19 +1,26 @@
 <?php 
 
 
-
 //  Ensure we are using the blog we want to. 
 Route::group(['namespace' => 'Thinkcreative\Blog\Controllers'], function() {
+
+	// Get a single specified blog post
+	Route::get('blog/{$slug}', ['uses' => 'BlogController@show']);
 
 	// Get the blog posts. We dont need to do anything else here. 
     Route::get('blog', ['uses' => 'BlogController@index']);
 
 });
 
+
+/**
+ * Admin routes
+ */
+
 //  Ensure we are using the blog we want to. 
-Route::group(['namespace' => 'Thinkcreative\Blog\Controllers', 'prefix' => 'admin'], function() {
+Route::group(['namespace' => 'Thinkcreative\Blog\Admin\Controllers', 'prefix' => 'admin'], function() {
 
 	// Get the blog posts. We dont need to do anything else here. 
-    Route::get('blog', ['uses' => 'BlogController@index']);
+    Route::resource('blog', ['uses' => 'BlogController@index']);
 
 });
