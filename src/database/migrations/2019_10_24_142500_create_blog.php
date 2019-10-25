50a8b2e01e97516e45a2c7b9ed932b8041821b03
thinkcreative-matt <56924270+thinkcreative-matt@users.noreply.github.com>
2019-10-25 12:09:06 +0100

    Change status and update controllers

---
+++ src/database/migrations/2019_10_24_142500_create_blog.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned()
+            $table->string('title', 100);    
+            $table->text('intro')->nullable();
+            $table->longText('body');
+            $table->enum('status', ['published', 'unpublished', 'draft']);
+            $table->timestamp('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
