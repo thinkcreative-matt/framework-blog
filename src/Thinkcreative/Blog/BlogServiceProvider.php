5c80c9611ffafd77adfea91b7b9499769be2acfa
thinkcreative-matt <56924270+thinkcreative-matt@users.noreply.github.com>
2019-10-28 13:15:55 +0000

    fix route loading

--- src/Thinkcreative/Blog/BlogServiceProvider.php
+++ src/Thinkcreative/Blog/BlogServiceProvider.php
@@ -38,7 +38,7 @@ class BlogServiceProvider extends ServiceProvider
 
 		$this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
 
-		$this->loadViewsFrom(__DIR__.'../../resources/views', 'blog');
+		$this->loadViewsFrom(__DIR__.'/../../resources/views', 'blog');
 
 		$this->publishes([
             __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/blog')
